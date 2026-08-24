612c40e38b33a3ebe33f8b7d2e08d2ce7693c0f5
Rybin <114862802+Guthuberik@users.noreply.github.com>
2026-08-24 12:43:37 +0300

    Update callback.js

--- api/auth/steam/callback.js
+++ api/auth/steam/callback.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  const steamId =
+    req.query["openid.claimed_id"]?.split("/").pop();
 
+  if (!steamId) {
+    return res.status(400).send("Steam ID not found");
+  }
+
+  res.status(200).send(`
+    <h1>Steam авторизация работает!</h1>
+    <p>Steam ID: ${steamId}</p>
+    <a href="/">Вернуться в MineGrade</a>
+  `);
+}
