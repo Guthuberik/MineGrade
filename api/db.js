5f479fa81133cf7c7bb34fa948c3015c7e3a0fc5
Rybin <114862802+Guthuberik@users.noreply.github.com>
2026-08-24 18:08:26 +0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,5 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.postgresql://neondb_owner:npg_HoThvIsB1pQ7@ep-frosty-surf-avgz4i0k-pooler.c-11.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require);
 
 export default sql;
