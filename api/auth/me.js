77351b1cd5d944c512a37efa003dc62fcfefbbea
Rybin <114862802+Guthuberik@users.noreply.github.com>
2026-08-24 18:21:56 +0300

    Update me.js

--- api/auth/me.js
+++ api/auth/me.js
@@ -2,9 +2,7 @@ import sql from "../../db.js";
 
 export default async function handler(req, res) {
   try {
-    const result = await sql`
-      SELECT 1 AS test
-    `;
+    const result = await sql`SELECT 1 AS test`;
 
     return res.status(200).json({
       ok: true,
